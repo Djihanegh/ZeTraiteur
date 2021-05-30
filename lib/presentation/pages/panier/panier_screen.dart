429c8ac92c362bca1a441f4e4dbc00aae0e25aba
Djihanegh <djihaneghilani@gmail.com>
2021-05-30 19:21:47 +0200

    finished the menu list

--- lib/presentation/pages/panier/panier_screen.dart
+++ lib/presentation/pages/panier/panier_screen.dart
@@ -20,30 +20,42 @@ class _PanierState extends State<Panier> {
         ),
         body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
           Padding(
-            padding: EdgeInsets.only(top: 20, right: 20),
+            padding: EdgeInsets.only(top: 20, right: 0),
             child: ListTile(
                 title: Text("Lieu de livraison"),
                 subtitle: TextField(),
                 trailing: Icon(Icons.mode_edit)),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 20, right: 20),
+            padding: EdgeInsets.only(top: 20, right: 0),
             child: ListTile(
                 title: Text("Numero de telephone"),
                 subtitle: TextField(),
                 trailing: Icon(Icons.mode_edit)),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 20, right: 270),
+            padding: EdgeInsets.only(top: 20, right: 290),
             child: Text("Repas:"),
           ),
-          ListView(children: [
-            ListTile(
-              title: Text('Commande classique'),
-              subtitle: Text('Ingredients'),
-              trailing: Icon(Icons.mode_edit),
-            )
-          ])
+          Expanded(child: 
+           ListView.builder(
+             
+             itemCount: 3,
+             itemBuilder: (context, index) {
+         return Column(children: [
+        
+        ListTile(
+            title: Text('Commande classique'),
+            subtitle: Text('Ingredients'),
+            trailing: Icon(Icons.mode_edit),
+          ),
+         Padding(
+            padding: EdgeInsets.only( right: 20), child:   Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [SizedBox(), Text("800 DA ")],
+          ) )
+          ],);
+        }))
         ]));
   }
 }
